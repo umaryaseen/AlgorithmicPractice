82b6843c7929b16c1b4ce40ff3b57540c83239d0
umaryaseen <umar.yaseen66.uy@gmail.com>
2024-09-28 12:20:00 +0000

    feat: add problem of the day for 2024-09-28

---
+++ leetcode/641/solution.py
@@ -0,0 +1,80 @@
+class MyCircularDeque:
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the deque to be k.
+        """
+        self.capacity = k
+        self.size = 0
+        self.front = 0
+        self.rear = -1
+        self.deque = [None] * k
+
+    def insertFront(self, value: int) -> bool:
+        """
+        Adds an item at the front of Deque. Return true if the operation is successful.
+        """
+        if self.isFull():
+            return False
+        self.rear = (self.rear + 1) % self.capacity
+        self.deque[self.rear] = value
+        self.size += 1
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        """
+        Adds an item at the rear of Deque. Return true if the operation is successful.
+        """
+        if self.isFull():
+            return False
+        self.rear = (self.rear + 1) % self.capacity
+        self.deque[self.rear] = value
+        self.size += 1
+        return True
+
+    def deleteFront(self) -> bool:
+        """
+        Deletes an item from the front of Deque. Return true if the operation is successful.
+        """
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return True
+
+    def deleteLast(self) -> bool:
+        """
+        Deletes an item from the rear of Deque. Return true if the operation is successful.
+        """
+        if self.isEmpty():
+            return False
+        self.rear = (self.rear - 1 + self.capacity) % self.capacity
+        self.size -= 1
+        return True
+
+    def getFront(self) -> int:
+        """
+        Get the front item from the deque.
+        """
+        if self.isEmpty():
+            return -1
+        return self.deque[self.front]
+
+    def getRear(self) -> int:
+        """
+        Get the last item from the deque.
+        """
+        if self.isEmpty():
+            return -1
+        return self.deque[self.rear]
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular deque is empty or not.
+        """
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular deque is full or not.
+        """
+        return self.size == self.capacity
